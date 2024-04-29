d1c4cc2de55c6db882db05bea1082f149131776d
John C. Barr <Farwalker3@icloud.com>
2024-04-29 18:57:09 -0400

    Alpha V4.1 (N-003)
    Alpha V4.1 (Nightly Update 003): Your Place For Everything

--- KodEDU/LogicCalc/LogicCalc.js
+++
@@ -1,5 +0,0 @@
-var editable = document.getElementById('English');
-var guess = document.getElementById('Guess');
-editable.addEventListener('input', function() {
-    guess.innerHTML = editable.innerHTML.replace('not', '¬').replace('and', '∧').replace('or', '∨').replace('nor', '⊕').replace('xor', '⊕').replace('then', '→').replace('are', '→').replace('if and only if', '↔');
-});
